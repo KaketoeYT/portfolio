037ab9f0c5cd232fb417c19945975cc8a5826a38
KaketoeYT <Thijsderogee@gmail.com>
2026-07-05 21:54:46 +0200

    made the title more personal, yoops

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Full Stack Dev | Portfolio' }}</title>
+    <title>{{ $title ?? 'Thijs Derogee | Portfolio' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link
